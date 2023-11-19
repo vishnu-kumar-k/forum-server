3a6b0d64c82eb90733b4ecfb3473fa237bdeb466
vibin-velumani <vibin04v@gmail.com>
2023-11-19 19:58:46 +0530

    feat : cors

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const express=require("express");
 const routes=require("./Routes");
 
 const app=express();
+const cors = require('cors');
+const bodyparser = require('body-parser');
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(
+    cors(
+        {
+            origin:"*",
+        }
+    )
+  );
 require("./db")
 require("dotenv").config()
 app.use(express.json());
