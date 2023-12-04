cbea39fed302b4072febc0fb9bd7dd578a15e19a
VasanthKathirvel <vasanthkathirkt@gmail.com>
2023-12-04 18:54:50 +0530

    1.2

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 mongoose.set("strictQuery",false);
-mongoose.connect("mongodb+srv://admin:admin@cluster0.nao0uqd.mongodb.net/?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://vishnuk55265:vishnuk55265@cluster0.svueomz.mongodb.net/?retryWrites=true&w=majority");
 const connection=mongoose.connection;
 
 connection.on("connected",()=>{
